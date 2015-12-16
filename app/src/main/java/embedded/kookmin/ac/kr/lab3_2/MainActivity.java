d4d66c2d12c0d6726c2154fc1dd27313eb9d7428
immune332 <immune332@gmail.com>
2015-12-16 18:38:48 +0900

    Initial commit

---
+++ app/src/main/java/embedded/kookmin/ac/kr/lab3_2/MainActivity.java
@@ -0,0 +1,51 @@
+package embedded.kookmin.ac.kr.lab3_2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btWeb;
+    Button btDial;
+    Button btPicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.web).setOnClickListener(mClickListener);
+        findViewById(R.id.dial).setOnClickListener(mClickListener);
+        findViewById(R.id.picture).setOnClickListener(mClickListener);
+    }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (v.getId()) {
+                case R.id.web:
+                    intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+                    startActivity(intent);
+                    break;
+                case R.id.dial:
+                    Toast.makeText(getApplicationContext(), "dial", Toast.LENGTH_LONG);
+                    break;
+                case R.id.picture:
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    Uri uri = Uri.fromFile(new File("/sdcard/test.jpg"));
+                    intent.setDataAndType(uri, "image/jpeg");
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
+}
